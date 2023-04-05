65a76b0448b0733fff6a3f06091fd1cf5d552a60
Kieszku94 <kieszku94@gmail.com>
2023-04-05 15:45:41 +0100

    Update getData.ts

--- src/data/getData.ts
+++ src/data/getData.ts
@@ -1,4 +1,26 @@
 import { Region } from "../enums/region";
 import { Gender } from "../enums/gender";
+import { getRandomInteger } from "../helpers/generateRandomInteger";
 
-export const getData = () => {};
+type UserModel = {
+  id: number;
+  birthday: number;
+  spend: number;
+  region: string;
+  gender: string;
+};
+
+export const getData = (): UserModel[] => {
+  const usersArray: UserModel[] = [];
+  for (let i = 1; i <= 1000; i++) {
+    const userObject: UserModel = {
+      id: i,
+      birthday: getRandomInteger(1, 12),
+      spend: getRandomInteger(0, 5000),
+      region: Region[getRandomInteger(1, 4)],
+      gender: Gender[getRandomInteger(1, 2)],
+    };
+    usersArray.push(userObject);
+  }
+  return usersArray;
+};
