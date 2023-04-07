85eca2ec321cd9b4df87f80ca75ae7aea204bc03
Kieszku94 <kieszku94@gmail.com>
2023-04-08 00:23:10 +0100

    Update Gender.tsx

--- src/components/Gender/Gender.tsx
+++ src/components/Gender/Gender.tsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gender.css";
 import { useSetGender } from "../../context/uTrackContext";
 
 const Gender = () => {
   const setGender = useSetGender();
+  const [maleActive, setMaleActive] = useState(false);
+  const [femaleActive, setFemaleActive] = useState(false);
+  const [allActive, setAllActive] = useState(true);
+
   const handleOnClick = (gender: string) => {
     setGender(gender);
   };
+
   return (
     <div className="genderContainer">
       <p className="genderLabel">Gender</p>
       <div className="genderButtonsGroup">
-        <button onClick={() => handleOnClick("Male")} className="genderBtn">
+        <button
+          onClick={() => {
+            setFemaleActive(false);
+            setAllActive(false);
+            setMaleActive(true);
+            handleOnClick("Male");
+          }}
+          className={maleActive ? "genderBtn active" : "genderBtn"}
+        >
           Male
         </button>
-        <button onClick={() => handleOnClick("Female")} className="genderBtn">
+        <button
+          onClick={() => {
+            setMaleActive(false);
+            setAllActive(false);
+            setFemaleActive(true);
+            handleOnClick("Female");
+          }}
+          className={femaleActive ? "genderBtn active" : "genderBtn"}
+        >
           Female
         </button>
-        <button onClick={() => handleOnClick("All")} className="genderBtn">
+        <button
+          onClick={() => {
+            setMaleActive(false);
+            setFemaleActive(false);
+            setAllActive(true);
+            handleOnClick("All");
+          }}
+          className={allActive ? "genderBtn active" : "genderBtn"}
+        >
           All
         </button>
       </div>
