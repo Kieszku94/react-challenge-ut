95a4137ad8c3b8deef91c5032deb3ad99c424b20
Kieszku94 <kieszku94@gmail.com>
2023-04-07 14:34:48 +0100

    Update Region.tsx

--- src/components/Region/Region.tsx
+++ src/components/Region/Region.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Region.css";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
@@ -14,12 +13,7 @@ const Region = () => {
       <p className="regionLabel">Select region</p>
       <div className="regionDropdown">
         <FormControl sx={{ minWidth: 200 }} size="small">
-          <InputLabel id="demo-select-small">Select region</InputLabel>
-          <Select
-            value={region}
-            label="Select region"
-            onChange={(e) => setRegion(e.target.value)}
-          >
+          <Select value={region} onChange={(e) => setRegion(e.target.value)}>
             <MenuItem value="United States">United States</MenuItem>
             <MenuItem value="Europe">Europe</MenuItem>
             <MenuItem value="APAC">APAC</MenuItem>
