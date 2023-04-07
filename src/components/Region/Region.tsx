6b9422610483a1dffb75b93d7c2c5cca506d660e
Kieszku94 <kieszku94@gmail.com>
2023-04-07 13:55:00 +0100

    Create Region.tsx

---
+++ src/components/Region/Region.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Region.css";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useRegion, useSetRegion } from "../../context/uTrackContext";
+
+const Region = () => {
+  const region = useRegion();
+  const setRegion = useSetRegion();
+  return (
+    <div className="regionContainer">
+      <p className="regionLabel">Select region</p>
+      <div className="regionDropdown">
+        <FormControl sx={{ minWidth: 200 }} size="small">
+          <InputLabel id="demo-select-small">Select region</InputLabel>
+          <Select
+            value={region}
+            label="Select region"
+            onChange={(e) => setRegion(e.target.value)}
+          >
+            <MenuItem value="United States">United States</MenuItem>
+            <MenuItem value="Europe">Europe</MenuItem>
+            <MenuItem value="APAC">APAC</MenuItem>
+            <MenuItem value="Latin America">Latin America</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
+};
+
+export default Region;
