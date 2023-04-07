9000f2526dc3f459d08271c64e52f06ff6fac7c3
Kieszku94 <kieszku94@gmail.com>
2023-04-07 13:54:49 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import MinimalSpend from "./components/MinimalSpend/MinimalSpend";
 import Gender from "./components/Gender/Gender";
+import Region from "./components/Region/Region";
 function App() {
   return (
     <div className="container">
       <MinimalSpend />
+      <Region />
       <Gender />
     </div>
   );
